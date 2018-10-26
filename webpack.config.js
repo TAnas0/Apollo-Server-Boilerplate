020028cc64ade6527c6f8a1ac300fc5cdca1973f
Anas Tiour <anas@cedrus.ma>
2018-10-26 02:12:58 +0100

    Webpack: ESLint, plugins and optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 // TODO: resolve @ alias
 // TODO: plugins
+// TODO: Depending on mode: Last part in here https://webpack.js.org/concepts/mode/
 const path = require('path');
+const webpack = require('webpack');
 const nodeExternals = require('webpack-node-externals');
 
 
@@ -9,8 +11,22 @@ module.exports = {
   target: 'node',
   externals: [nodeExternals()],
 
+  // Change this if you want to keep your code confidential
+  // See: https://blog.scottlogic.com/2017/11/01/webpack-source-map-options-quick-guide.html
+  devtool: "#source-map",
+
   module: {
     rules: [
+      // https://github.com/webpack-contrib/eslint-loader
+      {
+        test: /\.(js)$/,
+        loader: 'eslint-loader',
+        enforce: 'pre',
+        include: [path.resolve(__dirname, 'src'), path.resolve(__dirname, 'test')],
+        options: {
+          formatter: require('eslint-friendly-formatter')
+        }
+      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
@@ -30,5 +46,15 @@ module.exports = {
     alias: {
       '@': path.resolve(__dirname, 'src')
     }
+  },
+
+  plugins: [
+    // HMR should never be used in production
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+
+  optimization: {
+    // namedModules: true, // Not set, since used in dev mode and not in pro
+    noEmitOnErrors: true
   }
 };
