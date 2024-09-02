8dee63ba135092603eae557284201f32d6443c09
TAnas0 <anastiour@gmail.com>
2024-09-02 10:55:51 +0100

    style(src): correct ESLint errors

--- src/api/modules/auth/index.ts
+++ src/api/modules/auth/index.ts
@@ -1,10 +1,8 @@
 import { GraphQLModule } from "@graphql-modules/core"
 import { mergeTypes, mergeResolvers } from "merge-graphql-schemas"
-
 const authType = require("@/api/modules/auth/types/auth.graphql")
 import { authResolvers } from "@/api/modules/auth/resolvers/auth"
 import { tradeTokenForUser } from "./utils"
-
 
 const typeDefs = mergeTypes([authType])
 const resolvers = mergeResolvers([authResolvers])
